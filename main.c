934c2d90a3b3518efe97f02a871b9c2fba67969a
Jason Pearson <jjpearson8611@gmail.com>
2014-03-04 15:32:38 -0500

    Created General Things and Reading from the data file

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <string.h>
 
 //Data is stored in the bodyinfo array as
 // XCoord, YCoord, XVel, YVel, BodyMass
@@ -12,16 +13,19 @@ int numberOfBodies;
 
 //Prototypes
 void MoveABody(int BodySpotInArray);
-
+int TwoBodyCompare(int FirstBody, int SecondBody);
+void FillArrayFromFile();
 
 //main code
 int main(int argc, char * argv[])
 {
-	//once we get the file reading part done change 4 to the first line of the file
-	BodyInfo = (float *) malloc(5 * 4 * sizeof(float));
 
-	numberOfBodies = 4;
 	
+	//once we get the file reading part done change 4 to the first line of the file
+	BodyInfo = (float *) malloc(5 * numberOfBodies * sizeof(float));
+
+	FillArrayFromFile();	
+
 	int i;
 	for(i = 0; i < numberOfBodies; i++)
 	{
@@ -29,6 +33,7 @@ int main(int argc, char * argv[])
 	}
 
 }
+
 void MoveABody(int BodySpotInArray)
 {
 	//in this method we will compare each body with all the other bodies
@@ -40,4 +45,48 @@ void MoveABody(int BodySpotInArray)
 			printf("comparing %i and %i\n",BodySpotInArray, i);
 		}
 	}
+}
+
+int TwoBodyCompare(int FirstBody, int SecondBody)
+{
+
+
+
+
+}
+
+void FillArrayFromFile()
+{
+	FILE * ifp;
+	ssize_t read;
+	size_t len = 0;
+
+	ifp = fopen("Dataset","r");
+
+	char * inputLine = NULL;
+	char * splitLine;
+
+	int FirstRead = 1;
+	int NextOpenSpot = 0;
+	
+	while((read = getline(&inputLine, &len, ifp)) != -1)
+	{
+		if(FirstRead == 1)
+		{
+			FirstRead = 0;
+			numberOfBodies = atoi(inputLine);
+		}
+		else
+		{
+			splitLine = strtok(inputLine, ",");
+			BodyInfo[NextOpenSpot + 0] = (double) atoi(&splitLine[0]);
+			BodyInfo[NextOpenSpot + 1] = (double) atoi(&splitLine[2]);
+			BodyInfo[NextOpenSpot + 2] = (double) atoi(&splitLine[4]);
+			BodyInfo[NextOpenSpot + 3] = (double) atoi(&splitLine[6]);
+			BodyInfo[NextOpenSpot + 4] = (double) atoi(&splitLine[8]);
+			printf("XCoord %f, YCoord %f, XVel %f, YVel %f, Mass %f\n", BodyInfo[NextOpenSpot], BodyInfo[NextOpenSpot + 1],BodyInfo[NextOpenSpot + 2],BodyInfo[NextOpenSpot + 3],BodyInfo[NextOpenSpot + 4]);
+			NextOpenSpot += 5;
+		}
+	}
+	fclose(ifp);
 }
